f4852db29ee4bd54f1ef63ff012b3a941cdf005c
PetrovaAnastasia <petrova.anastasiya.dm@gmail.com>
2021-10-05 21:59:27 +0300

    Задание: Сверстать редактор на реакте
    Минимум к 15.10 статическая View, куда можно подставлять данные из 2 лабы.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Content(){
+    return (
+        <div>Content</div>
+    )
+}
+
+function Sidebar(){
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+function Menu(){
+    return (
+        <div>Menu</div>
+    )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div className="App">
+           <div><Sidebar></Sidebar></div>
+           <div><Content></Content></div>
+       </div>
+       <div>
+       </div>
       </header>
     </div>
   );
