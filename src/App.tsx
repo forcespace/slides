2d7d88e4d0611c937030cb6b760e56279e597e76
forcespace <force.space@rambler.ru>
2021-10-21 00:49:12 +0300

    20-10-21 - новая структура, верстка

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Workspace} from "./components/Workspace";
 
 import './App.css';
 import {editor} from "./script/slide/editor";
+import {Footer} from "./components/Footer";
 
 
 export function App() {
@@ -13,6 +14,7 @@ export function App() {
         <div className={'b-presentation'}>
             <Header title={presentation.title}/>
             <Workspace slides={presentation.slides}/>
+            <Footer/>
         </div>
     );
 }
