f6f2f9f324d34122256ccc83fc4dc3427e0a05d4
forcespace <force.space@rambler.ru>
2021-11-15 17:48:48 +0300

    14-11-21-s добавил css модули

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {SlideList} from "../SlidesList";
 import {SlideContent} from "../SlideContent";
 import {Editor} from "../../script/slide/slide";
-import '../../style/block/workspace/workspace.css';
+import styles from './workspace.module.css'
 
 export function Workspace(props: Editor)
 {
     const slidesCount = props.presentation.slides.length;
 
     return (
-        <section className={'b-presentation__workspace'}>
+        <section className={styles.workspace}>
             <SlideList {...props}/>
             <>
                 {slidesCount ? (<SlideContent slide={props.presentation.slides[props.active]} isScale={false}/>) : null}
