5e96e146e6363fab8715908525136c5f29ce06ae
forcespace <force.space@rambler.ru>
2022-01-25 12:16:38 +0300

    25-01-22-s Консоли логи удалил

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,10 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Nav from './components/Navigation/Nav'
 import {connect} from 'react-redux'
-// import { store } from './script/slide/store'
-// import { Editor } from './script/slide/slide'
 
 const connector = connect(null)
 
 export function App() {
-    // const state: Editor = store.getState()
-    // console.log('CurrentColor = ', state.presentation.color)
 
     return (
         <div className={'presentation'}>
